70ddd2ae0d384ed667d782e59a05780e9165c475
MaulanaMadd <djikodotkom@gmail.com>
2025-05-22 15:48:11 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 
 
@@ -8,13 +11,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('dosen',[DosenController::class,'index']);
+Route::get('welcome',[DosenController::class,'welcome']);
+Route::get('/pegawai/{nama}',[PegawaiController::class,'index'],'PegawaiController@index');
+Route::get('/formulir',[PegawaiController::class,'formulir']);
+Route::post('/formulir/proses',[PegawaiController::class,'proses']);
+
+// route blog
+Route::get('/blog',[BlogController::class,'home']);
+Route::get('/blog/tentang',[BlogController::class,'tentang']);
+Route::get('/blog/kontak',[BlogController::class,'kontak']);
+
 Route::get('halo', function () {
 	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
 });
 
-Route::get('blog', function () {
+/*Route::get('blog', function () {
 	return view('blog');
-});
+});*/
 
 Route::get('pertama', function () {
 	return view('pertama');
